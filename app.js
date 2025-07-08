befd3292940cdf70434bc2ebe01f40016ed75f90
EduardaBedetti <bedettieduarda29@gmail.com>
2025-07-08 20:49:44 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+//questão 1 corrigida
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+
+//questao 2
+let NumeroDigitado = prompt("Digite um numero");
+
+if (NumeroDigitado >= 0) {
+    alert("Positivo");
+}   else{
+    alert("numero negativo");
+}
+
+//questão 3
+let pontuacao = 50;
+
+if (pontuacao >= 100) {
+    console.log("Parabéns você venceu!");
+}   else {
+    console.log("Tente novamente para ganhar");
+}
+
+//questão 4
+let saldoConta = 500; 
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+//questão 5
+let Nome = prompt("Qual o seu nome?");
+alert(`Boas-vindas ${Nome}`);
